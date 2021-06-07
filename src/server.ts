e9a67f5edae0ea0fd3ffd310e66441e132c1d84b
luise-go <luise.goede@yahoo.de>
2021-06-07 10:26:30 +0200

    :sparkles: Serve storybook

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,9 @@ import express from 'express';
 console.log(fs.readFileSync('./README-md', 'utf-8'));
 
 const app = express();
+
+// Serve storybook production bundle
+app.use('/storybook', express.static('dist/storybook'));
+
 // Serve app production bundle
 app.use(express.static('dist/app'));
